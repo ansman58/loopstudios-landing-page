7a9b4bb6a31f0e11349b869c1dab3ce57fd2f071
ansman58 <javanslem@gmail.com>
2023-08-10 15:38:14 +0100

    fix

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -21,8 +21,8 @@ const HeroSection = () => {
     >
       <Menu className="mb-[50px]" />
       <section className="px-[2rem] md:px-[50px] lg:px-[100px]">
-        <article className="border-[1px] border-solid p-[1.875rem] border-white md:p-[1.6rem] max-w-[32rem] lg:max-w-[44rem] lg:p-[2.65rem_2.65rem_2rem] opacity-[0.9] my-auto">
-          <h1 className="text-white text-[36px] lg:text-[3.5rem] lg:leading-[97%]">
+        <article className="border-2 border-solid p-[28px] md:p-[1.6rem] border-white max-w-[32rem] lg:max-w-[44rem] lg:p-[2.65rem_2.65rem_2rem] opacity-[0.9] my-auto">
+          <h1 className="text-white text-[1.75rem] md:text-[2.25rem] lg:text-[3.5rem] lg:leading-[97%]">
             IMMERSIVE EXPERIENCES THAT DELIVER
           </h1>
         </article>
