7710eb3ec04f63b0bb544125ec1567888e53bc85
ansman58 <javanslem@gmail.com>
2023-08-10 16:29:05 +0100

    fix

--- app/components/ImageCard.tsx
+++ app/components/ImageCard.tsx
@@ -9,14 +9,14 @@ interface IImageCard {
 const ImageCard = (props: IImageCard) => {
   return (
     <div className="relative">
-    <figure className="mb-[1rem] md:mb-0 relative w-[100] block ">
+    <div className="mb-[1rem] md:mb-0 relative w-[100] block ">
       <Image src={props.backgroundImage} alt="" className="object-cover w-full max-w-full"/>
 
       <article className="absolute pl-[20px] bottom-[1rem] text-white text-25 md:text-[18px] lg:text-25">
         <h2 className="text-left">{props.text1}</h2>
         <h2>{props.text2}</h2>
       </article>
-    </figure>
+    </div>
     <div className="absolute inset-0 bg-black opacity-[0.2]"></div>
     </div>
 
